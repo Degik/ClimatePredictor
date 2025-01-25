44b5d5990b881a9f54471549ccfd98eb04602237
Degik <prodax9900@gmail.com>
2025-01-25 13:07:37 +0100

    Added nodes

--- FederateServer.py
+++ FederateServer.py
@@ -14,7 +14,17 @@ path_node = "/home/ubuntu/davide_b/ClimatePredictor_RL_FL/dataset/stations/"
 
 nodes = [
     Node.options(resources={"n12": 1}).remote(node_id=0, local_data_path=path_node + "1.csv"),
-    Node.options(resources={"n13": 1}).remote(node_id=1, local_data_path=path_node + "2.csv")
+    Node.options(resources={"n12": 1}).remote(node_id=1, local_data_path=path_node + "2.csv"),
+    Node.options(resources={"n12": 1}).remote(node_id=2, local_data_path=path_node + "3.csv"),
+    Node.options(resources={"n12": 1}).remote(node_id=3, local_data_path=path_node + "4.csv"),
+    Node.options(resources={"n13": 1}).remote(node_id=4, local_data_path=path_node + "5.csv"),
+    Node.options(resources={"n13": 1}).remote(node_id=5, local_data_path=path_node + "6.csv"),
+    Node.options(resources={"n13": 1}).remote(node_id=6, local_data_path=path_node + "7.csv"),
+    Node.options(resources={"n13": 1}).remote(node_id=7, local_data_path=path_node + "8.csv"),
+    Node.options(resources={"n14": 1}).remote(node_id=8, local_data_path=path_node + "9.csv"),
+    Node.options(resources={"n14": 1}).remote(node_id=9, local_data_path=path_node + "10.csv"),
+    Node.options(resources={"n14": 1}).remote(node_id=10, local_data_path=path_node + "11.csv"),
+    Node.options(resources={"n14": 1}).remote(node_id=11, local_data_path=path_node + "12.csv")
 ]
 
 aggregator = FederatedAggregator.remote(nodes)
